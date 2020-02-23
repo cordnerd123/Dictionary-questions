b85cd83cf6f5f0ab16e6a5526953cf32b0eed4bf
Dan Cordner <takamine16@gmail.com>
2020-02-22 19:15:26 -0500

    done

--- Dictionary questions/Program.cs
+++ Dictionary questions/Program.cs
@@ -8,35 +8,35 @@ namespace Dictionary_questions
     {
         static void Main(string[] args)
         {
-           // int[] nums1 = { 1, 2, 2, 1 };
-            //int[] nums2 = { 2, 2 };
-            //Console.WriteLine("Question 5-Part 2");
-            //int[] intersect2 = Intersect2(nums1, nums2);
-            //Console.WriteLine("Part 2- Intersection of two arrays is: ");
-            //DisplayArray(intersect2);
-            //Console.WriteLine("\n");
+            int[] nums1 = { 1, 2, 2, 1 };
+            int[] nums2 = { 1, 2, 2 };
+            Console.WriteLine("Question 5-Part 2");
+            int[] intersect2 = Intersect2(nums1, nums2);
+            Console.Write("Part 2- Intersection of two arrays is: ");
+            Console.Write("[" + String.Join(",",intersect2.Select(p => p.ToString())) + "]");
+            Console.WriteLine("\n");
 
         }
         public static int[] Intersect2(int[] nums1, int[] nums2)
         
         {
-            try
-            {
-                string[] vars1 = nums1.ToString;
-                string[] vars2 = nums2.ToString;
-                IEnumerable<string> query = from var in vars1.Intersect(vars2)
-                                            select var;
+            var primarydict = nums1.Select((index, value) => new { value, index })
+                      .ToDictionary(pair => pair.value, pair => pair.index);
 
-                foreach (var str in query)
-                {
-                    Console.WriteLine(str);
+            var secondarydict = nums2.Select((index, value) => new { value, index })
+                      .ToDictionary(pair => pair.value, pair => pair.index);
 
-                }
+            //outputs index of intersection in terms of nums1
+            var resultDict = primarydict.Keys.Intersect(secondarydict.Keys)
+                                          .ToDictionary(t => t, t => primarydict[t]);
 
-            catch
-            {
+            int x = resultDict.Count;
+            int[] retrn = new int[x];
 
-            }
+            for (int i=0; i<x;i++)
+                retrn[i] = resultDict[i];
+
+            return retrn;
         }
 
     }
